df8d458cec03212bcf11fef67ca91d7767cd748f
Serg Proph <ProphSerg@gmail.com>
2022-10-19 14:49:41 +0600

    19/10/2022 14:50

--- cgi-bin/trend.py
+++ cgi-bin/trend.py
@@ -3,6 +3,7 @@
 import requests
 import json
 import instrument
+import os
 
 settingISS = {
     'stock': {
@@ -35,7 +36,7 @@ def getPrc(a, b):
 def getTrend(bid, offer):
     if bid is None or offer is None:
         return ''
-    return '<span style="color:%s";>%s' %('green' if bid > offer else 'red', 'к покупке' if bid > offer else 'к продаже')
+    return inTAG('span', 'к покупке' if bid > offer else 'к продаже', param=['style="color:%s";' %('green' if bid > offer else 'red'),], NL=False)
 
 def inTAG(tag, data, param=None, NL=True):
     return '<%s%s>%s</%s>%s' %(
@@ -50,7 +51,9 @@ for e in settingISS:
     for m in settingISS[e]:
         jar = requests.cookies.RequestsCookieJar()
         # установка cookie `tasty_cookie=yum` на путь `httpbin.org/cookies`
-        #jar.set('MicexPassportCert', '1664453989007', domain='.moex.com', path='/')
+        jar.set('MicexPassportCert', os.getenv('MOEX_AUTH'),
+                #domain='.moex.com', path='/'
+                )
         
         req = requests.get(URL_PREF + 'iss/engines/%s/markets/%s/securities.json' %(e, m),
             params = {
@@ -74,7 +77,8 @@ sortBy = {}
 sortBy['VOLTODAY'] = sorted(data['marketdata'], key=lambda md: md['VOLTODAY'], reverse=True)
 sortBy['VALTODAY'] = sorted(data['marketdata'], key=lambda md: md['VALTODAY'], reverse=True)
 
-print('<html lang="ru_RU"><head><meta charset="UTF-8">', flush=True)
+print('Content-Type: text/html\n')
+print('<!DOCTYPE html><html lang="ru_RU"><head><meta charset="UTF-8">', flush=True)
 print(inTAG('style', 'table {border-collapse: collapse; border: 1px solid black;}'))
 print('</head><body>')
 
@@ -83,7 +87,7 @@ print(inTAG('h3', 'Загружено: %d инструменов' %len(data['mar
 
 print('<table border="1" style="border: 2px solid blue;"><tr>')
 for s in sortBy:
-    print(inTAG('th', 'Сортировка по: %s' %s, param=['style="text-align: center;"',]))
+    print(inTAG('th', 'Сортировка по: %s' %s, param=['style="text-align: center;"',], NL=False))
 print('</tr><tr>')
 
 for s in sortBy:
@@ -91,41 +95,58 @@ for s in sortBy:
     print( \
         '<td><table border="1">' +
         inTAG('tr', ''.join(list(
-            map(lambda c: inTAG('th', c, param=['style="text-align: center;"',], NL=False), col + ['BID %%', 'OFFER%%', 'Тенденция'])
-        )))
+            map(lambda c: inTAG('th', c, param=['style="text-align: center;"',], NL=False), col +
+                ['BID %%', 'OFFER %%', 'Тенденция', 'BIDT %%', 'OFFERT %%', 'Тенденция'])
+        )), NL=False)
     )
     tot = {
+        'BIDDEPTH': 0,
         'BIDDEPTHT': 0,
+        'OFFERDEPTH': 0,
         'OFFERDEPTHT': 0,
     }
     for i in range(15):
-        bid = getPrc(sortBy[s][i]['BIDDEPTHT'], sortBy[s][i]['BIDDEPTHT'] + sortBy[s][i]['OFFERDEPTHT'])
-        offer = getPrc(sortBy[s][i]['OFFERDEPTHT'], sortBy[s][i]['BIDDEPTHT'] + sortBy[s][i]['OFFERDEPTHT'])
-        tot['BIDDEPTHT'] = tot['BIDDEPTHT'] + sortBy[s][i]['BIDDEPTHT']
-        tot['OFFERDEPTHT'] = tot['OFFERDEPTHT'] + sortBy[s][i]['OFFERDEPTHT']
+        bid = getPrc(sortBy[s][i]['BIDDEPTH'], sortBy[s][i]['BIDDEPTH'] + sortBy[s][i]['OFFERDEPTH'])
+        offer = getPrc(sortBy[s][i]['OFFERDEPTH'], sortBy[s][i]['BIDDEPTH'] + sortBy[s][i]['OFFERDEPTH'])
+        bidt = getPrc(sortBy[s][i]['BIDDEPTHT'], sortBy[s][i]['BIDDEPTHT'] + sortBy[s][i]['OFFERDEPTHT'])
+        offert = getPrc(sortBy[s][i]['OFFERDEPTHT'], sortBy[s][i]['BIDDEPTHT'] + sortBy[s][i]['OFFERDEPTHT'])
+        for t in tot:
+            tot[t] = tot[t] + sortBy[s][i][t]
         print(inTAG('tr',
                 ''.join(list(map(lambda c: inTAG('td',sortBy[s][i][c], NL=False), col))) +
                 inTAG('td', '%s %%' %bid, NL=False) +
                 inTAG('td', '%s %%' %offer, NL=False) +
-                inTAG('td', getTrend(bid, offer), NL=False)
-            )
+                inTAG('td', getTrend(bid, offer), NL=False) +
+                inTAG('td', '%s %%' % bidt, NL=False) +
+                inTAG('td', '%s %%' % offert, NL=False) +
+                inTAG('td', getTrend(bidt, offert), NL=False)
+        , NL=False)
         )
 
-    bid = getPrc(tot['BIDDEPTHT'], tot['BIDDEPTHT'] + tot['OFFERDEPTHT'])
-    offer = getPrc(tot['OFFERDEPTHT'], tot['BIDDEPTHT'] + tot['OFFERDEPTHT'])
+    bid = getPrc(tot['BIDDEPTH'], tot['BIDDEPTH'] + tot['OFFERDEPTH'])
+    offer = getPrc(tot['OFFERDEPTH'], tot['BIDDEPTH'] + tot['OFFERDEPTH'])
+    bidt = getPrc(tot['BIDDEPTHT'], tot['BIDDEPTHT'] + tot['OFFERDEPTHT'])
+    offert = getPrc(tot['OFFERDEPTHT'], tot['BIDDEPTHT'] + tot['OFFERDEPTHT'])
     print(
         inTAG('tr',
-            inTAG('td', 'ИТОГО', param=['colspan=3',], NL=False) + inTAG('td', '', NL=False) +
-            inTAG('td', tot['BIDDEPTHT'], NL=False) + inTAG('td', '', NL=False) +
-            inTAG('tg', tot['OFFERDEPTHT'], NL=False) + inTAG('td', '', NL=False) +
+            inTAG('td', 'ИТОГО', param=['colspan=3',], NL=False) +
+            inTAG('td', tot['BIDDEPTH'], NL=False) +
+            inTAG('td', tot['BIDDEPTHT'], NL=False) +
+            inTAG('td', tot['OFFERDEPTH'], NL=False) +
+            inTAG('td', tot['OFFERDEPTHT'], NL=False) +
             inTAG('td', '%s %%' % bid, NL=False) +
             inTAG('td', '%s %%' % offer, NL=False) +
-            inTAG('td', getTrend(bid, offer), NL=False)
-        )
+            inTAG('td', getTrend(bid, offer), NL=False) +
+            inTAG('td', '%s %%' % bidt, NL=False) +
+            inTAG('td', '%s %%' % offert, NL=False) +
+            inTAG('td', getTrend(bidt, offert), NL=False)
+    , NL=False)
     )
 
     print('</table></td>')
 print('</tr></table>')
-print('<p><h3>BID %% = BIDDEPTHT / (BIDDEPTHT + OFFERDEPTHT) * 100%</h3>')
-print('<p><h3>OFFER %% = OFFERDEPTHT / (BIDDEPTHT + OFFERDEPTHT) * 100%</h3>')
+print(inTAG('h3', 'BID %% = BIDDEPTH / (BIDDEPTH + OFFERDEPTH) * 100%'))
+print(inTAG('h3', 'OFFER %% = OFFERDEPTH / (BIDDEPTH + OFFERDEPTH) * 100%'))
+print(inTAG('h3', 'BIDT %% = BIDDEPTHT / (BIDDEPTHT + OFFERDEPTHT) * 100%'))
+print(inTAG('h3', 'OFFERT %% = OFFERDEPTHT / (BIDDEPTHT + OFFERDEPTHT) * 100%'))
 print('</body></html>')
