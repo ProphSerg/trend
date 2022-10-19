b9cb9254b4e384c96cdece2936ca87d4147c887a
Serg Proph <ProphSerg@gmail.com>
2022-10-19 13:32:45 +0600

    19/10/2022 13:32

--- cgi-bin/trend.py
+++ cgi-bin/trend.py
@@ -51,6 +51,7 @@ for e in settingISS:
         jar = requests.cookies.RequestsCookieJar()
         # установка cookie `tasty_cookie=yum` на путь `httpbin.org/cookies`
         #jar.set('MicexPassportCert', '1664453989007', domain='.moex.com', path='/')
+        
         req = requests.get(URL_PREF + 'iss/engines/%s/markets/%s/securities.json' %(e, m),
             params = {
                 'iss.meta': 'off',
@@ -61,11 +62,11 @@ for e in settingISS:
             cookies = jar
         )
         j = req.json()
-        '''
+        
         for i in j['marketdata']['data']:
             if not (i[1] in instrument.instrument and i[0] in instrument.instrument[i[1]]):
                 j['marketdata']['data'].remove(i)
-        '''
+        
         #data['securities'] = data['securities'] + convertData(j['securities'])
         data['marketdata'] = data['marketdata'] + convertData(j['marketdata'])
 
@@ -73,7 +74,7 @@ sortBy = {}
 sortBy['VOLTODAY'] = sorted(data['marketdata'], key=lambda md: md['VOLTODAY'], reverse=True)
 sortBy['VALTODAY'] = sorted(data['marketdata'], key=lambda md: md['VALTODAY'], reverse=True)
 
-print('<html lang="ru_RU"><head><meta charset="UTF-8">')
+print('<html lang="ru_RU"><head><meta charset="UTF-8">', flush=True)
 print(inTAG('style', 'table {border-collapse: collapse; border: 1px solid black;}'))
 print('</head><body>')
 
@@ -90,7 +91,7 @@ for s in sortBy:
     print( \
         '<td><table border="1">' +
         inTAG('tr', ''.join(list(
-            map(lambda c: inTAG('th' c, param=['style="text-align: center;"',]), col + ['BID %%', 'OFFER%%', 'Тенденция'])
+            map(lambda c: inTAG('th', c, param=['style="text-align: center;"',], NL=False), col + ['BID %%', 'OFFER%%', 'Тенденция'])
         )))
     )
     tot = {
@@ -102,19 +103,24 @@ for s in sortBy:
         offer = getPrc(sortBy[s][i]['OFFERDEPTHT'], sortBy[s][i]['BIDDEPTHT'] + sortBy[s][i]['OFFERDEPTHT'])
         tot['BIDDEPTHT'] = tot['BIDDEPTHT'] + sortBy[s][i]['BIDDEPTHT']
         tot['OFFERDEPTHT'] = tot['OFFERDEPTHT'] + sortBy[s][i]['OFFERDEPTHT']
-        print('<tr>%s<td>%s %%</td><td>%s %%</td><td>%s</td></tr>' %(
-                ''.join(list(map(lambda c: '<td>%s</td>' %str(sortBy[s][i][c]), col))),
-                bid, offer,
-                getTrend(bid, offer)
+        print(inTAG('tr',
+                ''.join(list(map(lambda c: inTAG('td',sortBy[s][i][c], NL=False), col))) +
+                inTAG('td', '%s %%' %bid, NL=False) +
+                inTAG('td', '%s %%' %offer, NL=False) +
+                inTAG('td', getTrend(bid, offer), NL=False)
             )
         )
 
     bid = getPrc(tot['BIDDEPTHT'], tot['BIDDEPTHT'] + tot['OFFERDEPTHT'])
     offer = getPrc(tot['OFFERDEPTHT'], tot['BIDDEPTHT'] + tot['OFFERDEPTHT'])
-    print('<tr><td colspan=3>ИТОГО</td><td></td><td>%d</td><td></td><td>%d</td><td>%s %%</td><td>%s %%</td><td>%s</td></tr>' %(
-            tot['BIDDEPTHT'], tot['OFFERDEPTHT'],
-            bid, offer,
-            getTrend(bid, offer)
+    print(
+        inTAG('tr',
+            inTAG('td', 'ИТОГО', param=['colspan=3',], NL=False) + inTAG('td', '', NL=False) +
+            inTAG('td', tot['BIDDEPTHT'], NL=False) + inTAG('td', '', NL=False) +
+            inTAG('tg', tot['OFFERDEPTHT'], NL=False) + inTAG('td', '', NL=False) +
+            inTAG('td', '%s %%' % bid, NL=False) +
+            inTAG('td', '%s %%' % offer, NL=False) +
+            inTAG('td', getTrend(bid, offer), NL=False)
         )
     )
 
